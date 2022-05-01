949e0f79b7ff305dac2ad6a0798406a6ab25a892
mikedubcurry <michaelcurry95@gmail.com>
2022-05-01 16:08:26 -0400

    scanner kinda works

---
+++ src/lib/Scanner.svelte
@@ -0,0 +1,82 @@
+<script lang="ts">
+	import { onMount, createEventDispatcher } from 'svelte';
+	import { Html5Qrcode, Html5QrcodeScannerState } from 'html5-qrcode';
+
+	import type { Html5QrcodeSupportedFormats } from 'html5-qrcode';
+
+	interface Options {
+		codeType: Html5QrcodeSupportedFormats | Html5QrcodeSupportedFormats[];
+		scanArea?: {
+			width: number;
+			height: number;
+		};
+	}
+
+	export let options: Options;
+
+	let scanner: Html5Qrcode;
+	let cameraSupportError = false;
+
+	const dispatch = createEventDispatcher();
+
+	onMount(() => {
+		scanner = new Html5Qrcode('reader', {
+			formatsToSupport:
+				typeof options.codeType !== 'object' ? [options.codeType] : options.codeType,
+			verbose: false,
+		});
+
+		Html5Qrcode.getCameras()
+			.then((cameras) => {
+				scanner.start(
+					cameras[0].id,
+					{
+						fps: 10,
+						qrbox: {
+							width: options.scanArea?.width ?? 100,
+							height: options.scanArea?.height ?? 100,
+						},
+					},
+					(code) => {
+						dispatch('scan-success', code);
+					},
+					(err) => {
+						// console.log(err);
+					},
+				);
+			})
+			.catch((e) => {
+				console.log(e);
+				cameraSupportError = true;
+				// display to user that scanner is not supported
+			});
+
+		return () => {
+			if (scanner.getState() === Html5QrcodeScannerState.SCANNING) scanner.stop();
+		};
+	});
+</script>
+
+<div class="scanner">
+	<div id="reader">
+		<div class="spinner" />
+		{#if cameraSupportError}
+			Camera access is not supported by your device :(
+		{/if}
+	</div>
+</div>
+
+<style>
+	.spinner {
+		width: 30px;
+		height: 30px;
+		border-bottom: 1px solid blue;
+		animation: spin .5s linear infinite;
+        border-radius: 50%;
+	}
+	@keyframes spin {
+		to {
+			transform: rotate(360deg);
+		}
+	}
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,75 +1,27 @@
 <script lang="ts">
-	import { onMount } from 'svelte';
-	import { page } from '$app/stores';
-	import {
-		Html5QrcodeScanner,
-		Html5QrcodeSupportedFormats,
-		Html5QrcodeScanType,
-	} from 'html5-qrcode';
+	import Scanner from '$lib/Scanner.svelte';
+	import { Html5QrcodeSupportedFormats } from 'html5-qrcode';
 
-	let isbn = '';
-	let bookData = {};
-	let scanner: Html5QrcodeScanner;
+	let scannerVisible = false;
 
-	onMount(() => {
-		scanner = new Html5QrcodeScanner(
-			'reader',
-			{
-				formatsToSupport: [Html5QrcodeSupportedFormats.EAN_13],
-				fps: 10,
-				qrbox: { width: 250, height: 250 },
-				supportedScanTypes: [Html5QrcodeScanType.SCAN_TYPE_CAMERA],
-			},
-			false,
-		);
-		scanner.render(
-			(code) => {
-				console.log('setting isbn');
-				isbn = code;
-			},
-			(e) => {
-				// console.log(e);
-			},
-		);
-	});
-
-	async function getBookData() {
-		try {
-			const response = await fetch(`${$page.url.origin}/api/scan/${isbn}`);
-			if (response.ok) {
-				const data = await response.json();
-				bookData = data;
-				scanner.pause();
-			} else {
-				throw Error();
-			}
-		} catch (e) {
-			console.log('oops');
-
-			isbn = '';
-			bookData = {};
-		}
-	}
-	$: {
-		if (isbn) {
-			console.log(isbn);
-			getBookData();
-		}
+	function handleScan(code: string) {
+		console.log(code);
+		scannerVisible = false;
 	}
 </script>
 
-<div>
-	<h1>Library of Alexa</h1>
-</div>
 
-<div class="scanner">
-	<div id="reader" />
-	<pre>{JSON.stringify(bookData, null, 2)}</pre>
-</div>
+<button on:click={() => (scannerVisible = !scannerVisible)}
+	>{scannerVisible ? 'hide' : 'show'} scanner</button
+>
+{#if scannerVisible}
+	<h1>Scanner</h1>
 
-<style>
-    .scanner {
-        max-width: 600px;
-        width: 100%;
-    }
-</style>
+	<Scanner
+		options={{
+			codeType: Html5QrcodeSupportedFormats.EAN_13,
+			scanArea: { width: 200, height: 150 },
+		}}
+		on:scan-success={(e) => handleScan(e.detail)}
+	/>
+{/if}
